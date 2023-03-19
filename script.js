cdc78033008369c40680b9c54b3e98cdbcde22a8
adamjatim <adamrahmatilahi@gmail.com>
2023-03-19 11:12:55 +0700

    update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const googleSearchBtn = document.querySelector("#google-search-btn");
+const ddgSearchBtn = document.querySelector("#ddg-search-btn");
+
+const googleUrl = "https://www.google.com/search?q=";
+const ddgUrl = "https://duckduckgo.com/?q=";
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchTerm = searchInput.value;
+
+  if (searchTerm) {
+    const googleSearch = googleUrl + searchTerm;
+    const ddgSearch = ddgUrl + searchTerm;
+
+    if (googleSearchBtn === event.submitter) {
+      window.location.href = googleSearch;
+    } else if (ddgSearchBtn === event.submitter) {
+      window.location.href = ddgSearch;
+    }
+  }
+});
